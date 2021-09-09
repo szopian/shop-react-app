052b6f2dbbb83be2a4eebde16e15d67913e8ec19
szopian <szopiangreg@gmail.com>
2021-09-09 12:00:05 +0100

    add Router an Switch from react router dom and start work on the shop page

--- shop-app/src/App.js
+++ shop-app/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
-import HomePage from "./pages/homepage";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
