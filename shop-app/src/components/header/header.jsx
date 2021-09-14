2f490f7f29c698232b56640db63f9becedfacc25
szopian <szopiangreg@gmail.com>
2021-09-14 12:30:51 +0100

    add redux, implamated userReducer and userActions to header and app componemts

--- shop-app/src/components/header/header.jsx
+++ shop-app/src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.utils";
 
@@ -22,7 +23,7 @@ const Header = ({ currentUser }) => {
         </Link>
         {currentUser ? (
           <div className="option" onClick={() => auth.signOut()}>
-            SIGN OUT
+            SIGN&nbsp;OUT
           </div>
         ) : (
           <Link className="option" to="/signIn">
@@ -34,4 +35,8 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
