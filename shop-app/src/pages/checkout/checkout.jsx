59bffd409ea132f698b1ce99759d098e36f3c7c7
szopian <szopiangreg@gmail.com>
2021-09-28 16:52:48 +0100

    add checkout page

--- shop-app/src/pages/checkout/checkout.jsx
+++ shop-app/src/pages/checkout/checkout.jsx
@@ -28,6 +28,11 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
+    {cartItems.map((cartItem) => cartItem.name)}
+
+    <div className="total">
+      <span>TOTAL: ${total}</span>
+    </div>
   </div>
 );
 
