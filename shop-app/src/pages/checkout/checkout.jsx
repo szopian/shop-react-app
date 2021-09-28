6ea949697851239ce7a86b102c15c49cdd05eb7f
szopian <szopiangreg@gmail.com>
2021-09-28 16:49:42 +0100

    add work with chockout page

--- shop-app/src/pages/checkout/checkout.jsx
+++ shop-app/src/pages/checkout/checkout.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 
 import "./checkout.scss";
 
-const CheckoutPage = () => <div>CHECKOUT PAGE</div>;
+const CheckoutPage = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Desciption</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+  </div>
+);
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
